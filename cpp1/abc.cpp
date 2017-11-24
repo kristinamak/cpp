a2b71067b96eb873ed9a232d2f758ef87e317243
Kristina Makarova <kristina.mak99@gmail.com>
2017-11-24 17:28:50 +0300

    cpp labs

---
+++ cpp1/abc.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    int c = 0;
+
+    std::scanf("%d %d %d", &a, &b, &c);
+
+    a = a + b + c;
+
+    std::printf("%d\n", a);
+}    
---
+++ cpp1/hello_err.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main()
+{
+    std::coyt << "Hello, Kristina!" << std::endl;
+}
